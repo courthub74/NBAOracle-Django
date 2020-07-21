c0d45c363d368159e91bc9553666dd1b13327968
Courtney <cjacobs124@gmail.com>
2020-07-21 18:21:16 -0400

    Get the Dropdown Output

--- nbaoracle2/views.py
+++ nbaoracle2/views.py
@@ -10,6 +10,7 @@ def home(request):
 	# ATLANTA General Info 134880
 	hawksRE = requests.get("https://www.thesportsdb.com/api/v1/json/1/searchteams.php?t=Atlanta%20Hawks")
 	hawks_info = json.loads(hawksRE.content)
+	
 	# ATLANTA Last Game 134880
 	hawksLG = requests.get("https://www.thesportsdb.com/api/v1/json/4013017/eventslast.php?id=134880")
 	hawks_last = json.loads(hawksLG.content)
